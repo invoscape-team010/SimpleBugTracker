ad3e445d096a546a5570086c3c4877bb51ffd233
invoscape-team010 <team010@invoscape.com>
2014-08-09 12:06:51 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBugTracker::Application.config.secret_token = '48c571da02602ce869a4bed98f4c907cd8f4db3af87db27d678efab54cd373e70fb5cf9ce10f503c72d6a43a421204d2c46fbe6584ccc9d49d8ee06d35e3b1ce'
